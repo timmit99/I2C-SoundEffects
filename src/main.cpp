542ad4017fde4aad05baac738ffedfe6e5d4a953
[Timothy Abraham] <[Abrahamtr@verizon.net]>
2018-02-04 00:48:00 -0500

    Cleanup
    Cleaning up of code for use in ATOM

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,4 @@
-/***************************************************
-DFPlayer - A Mini MP3 Player For Arduino
- <https://www.dfrobot.com/index.php?route=product/product&product_id=1121>
- 
- ***************************************************
- This example shows the basic function of library for DFPlayer.
- 
- Created 2016-12-07
- By [Angelo qiao](Angelo.qiao@dfrobot.com)
- 
- GNU Lesser General Public License.
- See <http://www.gnu.org/licenses/> for details.
- All above must be included in any redistribution
- ****************************************************/
 
-/***********Notice and Trouble shooting***************
- 1.Connection and Diagram can be found here
- <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
- 2.This code is tested on Arduino Uno, Leonardo, Mega boards.
- ****************************************************/
 
 #include "Arduino.h"
 #include "SoftwareSerial.h"
@@ -29,7 +10,6 @@ int prevCode;
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
 
 void setup()
 {
@@ -38,11 +18,11 @@ void setup()
   Serial.begin(115200);
 
   mySoftwareSerial.begin(9600);
-  
+
   Serial.println();
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-  
+
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
@@ -50,7 +30,7 @@ void setup()
     while(true);
   }
   Serial.println(F("DFPlayer Mini online."));
-  
+
   myDFPlayer.volume(25);  //Set volume value. From 0 to 30
 
 }
@@ -63,8 +43,12 @@ void loop() {
   }
 }
 
-void receiveEvent() 
+void receiveEvent()
 {
-    code = Wire.read();    // receive byte as an integer
-}
 
+  while(Wire.available())    // slave may send less than requested
+  {
+    code = Wire.read();    // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+}
